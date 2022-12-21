c164ce12154828d8a40f1d6c8e7baaaee35a2ff1
Pramit Chandra <112652928+PramitChandra@users.noreply.github.com>
2022-12-21 07:58:18 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -16,16 +16,21 @@ def divide(x, y):
     return x / y
 
 
+def power(x, y):
+    return x**y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -41,10 +46,13 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
 
+        elif choice == '5':
+            print(num1, "^", num2, "=", power(num1, num2))
+
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (y/n): ")
-        if next_calculation == "no":
+        if next_calculation == "n":
             break
 
     else:
